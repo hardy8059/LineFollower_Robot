3e29afa295d6a35908587eb730883ca35d3397f0
Hardik Munjal <hardy96tech@gmail.com>
2017-07-28 20:44:11 +0530

    Check Wiring of motor
    On uploading this code and powering the arduino, wheels first move forwards and then backwards if motor is wired correctly.

---
+++ forward_motion.ino
@@ -0,0 +1,53 @@
+// Shield Pins Declaration 
+int dataPin = 8;        
+int latchPin = 12;
+int clockPin = 4;
+int en = 7;
+void setup()
+{
+    pinMode(dataPin, OUTPUT);     // Setting up the motor shield. 
+    pinMode(latchPin, OUTPUT);
+    pinMode(clockPin, OUTPUT);
+    pinMode(en, OUTPUT);    
+    digitalWrite(en, LOW); 
+ 
+ forward(); // This funtion for forward robot motion
+ 
+  
+ }
+
+void loop()
+{
+}
+
+void forward(void){
+        digitalWrite(latchPin, LOW);            
+        shiftOut(dataPin, clockPin, LSBFIRST, 3);
+        digitalWrite(latchPin, HIGH);  
+
+}
+
+void backward(void){
+        digitalWrite(latchPin, LOW);            
+        shiftOut(dataPin, clockPin, LSBFIRST, 164);
+        digitalWrite(latchPin, HIGH);  
+}
+
+void turn_left(void){
+        digitalWrite(latchPin, LOW);            
+        shiftOut(dataPin, clockPin, LSBFIRST, 161);
+        digitalWrite(latchPin, HIGH);  
+}
+
+void turn_right(void){
+        digitalWrite(latchPin, LOW);            
+        shiftOut(dataPin, clockPin, LSBFIRST, 38);
+        digitalWrite(latchPin, HIGH);  
+}
+
+void halt(void){
+        digitalWrite(latchPin, LOW);            
+        shiftOut(dataPin, clockPin, LSBFIRST, 32);
+        digitalWrite(latchPin, HIGH);  
+}
+
